26fa2b2c55bbc3fe4e5d2cc68de6fcc511f2c5e8
syntaxbinladen3 <tskforcests@gmail.com>
2026-02-03 20:36:06 +0100

    Update DNS.py

--- DNS.py
+++ DNS.py
@@ -4,7 +4,7 @@ import time
 import random
 import struct
 
-TARGET_IP = "192.168.1.1"  # YOUR ROUTER
+TARGET_IP = "62.109.121.42"  # YOUR ROUTER
 
 # ==================== COLORS ====================
 YELLOW = '\033[93m'
